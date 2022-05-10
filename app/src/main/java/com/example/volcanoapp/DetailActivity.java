0d4862e6df0835f2197f4de6dc475e2e8f636569
Kevin Gilbert Toding <122202002832@mhs.dinus.ac.id>
2022-05-10 17:08:50 +0800

    add detail volcanos

---
+++ app/src/main/java/com/example/volcanoapp/DetailActivity.java
@@ -0,0 +1,62 @@
+package com.example.volcanoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+public class DetailActivity extends AppCompatActivity {
+
+    TextView tvNama, tvBentuk, tvTinggi, tvEstimasi, tvGeo;
+    ImageView imgGunung;
+
+    String image, nama, bentuk, tinggi, estimasi, geolokasi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+
+        // Inisialisasi imageView dan TextView pada DetailActivity
+
+        tvNama          =   (TextView) findViewById(R.id.det_nama_gunung);
+        tvBentuk        =   (TextView) findViewById(R.id.det_bentuk_gunung);
+        tvTinggi        =   (TextView) findViewById(R.id.det_tinggi);
+        tvEstimasi      =   (TextView) findViewById(R.id.det_estimasi);
+        tvGeo           =   (TextView) findViewById(R.id.det_geo);
+        imgGunung       =   (ImageView) findViewById(R.id.det_gambar_gunung);
+
+
+        // Memanggil intent kemudian mengambil data yang telah dikirimkan menggunakan intent pada mainActivity
+
+
+        Intent intent = getIntent();
+        this.image      = intent.getStringExtra("gambar");
+        this.nama       =   intent.getStringExtra("nama_gunung");
+        this.bentuk     =   intent.getStringExtra("bentuk_gunung");
+        this.tinggi     =   intent.getStringExtra("tinggi_gunung");
+        this.estimasi   =   intent.getStringExtra("estimasi_letusan");
+        this.geolokasi  =   intent.getStringExtra("geolokasi");
+
+
+        // Settext data ke dalam textView
+
+
+        tvNama.setText(nama);
+        tvBentuk.setText(bentuk);
+        tvTinggi.setText(tinggi);
+        tvEstimasi.setText(estimasi);
+        tvGeo.setText(geolokasi);
+
+        // Load ImageView menggunakan Glide
+
+        Glide.with(DetailActivity.this)
+                .load(image)
+                .into(imgGunung);
+    }
+}
